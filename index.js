10a2a6f1743185664b67a8f26b927b0ed5cfc1fc
Ismael Lopez <lopez42512@gmail.com>
2021-08-11 15:47:12 -0400

    new password is agenerated when user press btn

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// the characters that will be used to generate password
 const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&'
+// password will be stored here, a new character will be added after every loop
 let password = '';
 
-for(let i = 0; i < 10; i++) {
+
+document.getElementById("passwordBtn").addEventListener("click", function() {
+    // clear password to get new password every btn click
+    password = ''
+
+    // loops 10 times and generates a random number that is used to select a character that is stored in password variable
+    for(let i = 0; i < 10; i++) {
+
+    // generates a random number
     let selector = Math.floor(Math.random() * characters.length)
+
+    // adds a character to password variable
     password += characters[selector]
-}
-console.log(password);
-document.getElementById('password').innerHTML = password
+    }
+
+    // displays password to html element
+    document.getElementById('password').innerHTML = password
+})
